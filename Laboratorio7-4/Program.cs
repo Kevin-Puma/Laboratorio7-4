41398b7650767a6322f19196b1f94bada43e80d5
User <User@DESKTOP-CRJD420>
2023-10-04 19:31:00 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio7-4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Ingresa un número: ");
+int numero = int.Parse(Console.ReadLine());
+int numeroAsteriscos = numero * 2;
+
+for (int i = 0; i < numero; i++)
+{
+    for (int j = 0; j < numeroAsteriscos; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
